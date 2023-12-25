f3acd8466d254a0a11967fd0d0e82146907a5cb7
Beedful <91648273+Beedful@users.noreply.github.com>
2023-12-25 21:16:09 +0800

    fixed navigation error

--- images/1/script.js
+++ images/1/script.js
@@ -1,9 +1,9 @@
-var txt = document.getElementById("txt");
 var btn = document.getElementById("btn");
 var img = document.getElementById("img");
 
 btn.addEventListener('click', function() {
-    if (txt.value == "test_pass") {
+    let answer = prompt('Enter password');
+    if (answer == atob("dGVzdF9wYXNz")) {
         img.removeAttribute("hidden");
     } else {
         alert("Wrong password!!")
